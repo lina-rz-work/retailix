564524f918c410e9c69907f21a53a5143f6e006c
Alina Mukhamatkhanova <lina.rz.work@gmail.com>
2024-06-26 18:53:01 +0500

    update build path

--- api/index.js
+++ api/index.js
@@ -47,10 +47,10 @@ app.use('/api/cart', cartRoutes);
 app.use('/api', productRoutes);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
